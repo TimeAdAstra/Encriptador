8bb4349c76dbb94c1518d88168c23d348e08de6f
TimeAdAstra <fenix_stylebf3@hotmail.com>
2023-01-30 17:48:35 -0600

    Se agrego la variable textoContenedor a JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+function encriptar(){
+    var texto = document.getElementById("textoEncriptado").value.toLowerCase();
+    
+             var textoEncriptado = texto.replace(/e/img, "enter");
+             var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+             var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+             var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+             var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+             textoEncriptado.value = "";
+            
+        document.getElementById("mensajeDesencriptado").innerHTML = textoEncriptado;
+        document.getElementById("btn-copiado").style.display = "show";
+        document.getElementById("btn-copiado").style.display = "inherit";
+        document.getElementById("mensajeDesencriptado").style.backgroundImage = "none";
+        
+    
+}
+    
+function desencriptar(){
+    var texto = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = texto.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("mensajeDesencriptado").innerHTML = textoEncriptado;
+}
+
+function copiar(){
+    var contenido = document.querySelector("#mensajeDesencriptado");
+    contenido.select();
+    document.execCommand("copy");
+}
